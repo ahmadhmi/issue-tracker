bea5fe78c912fa43b8ca5eef219627e8a80db168
Ahmad Heshmati <amd.heshmati@gmail.com>
2024-01-02 01:41:22 -0700

    Build the new issue page

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,12 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main>
           <Theme>
             <NavBar />
-            {children}
+            <main className="p-5">{children}</main>
           </Theme>
-        </main>
       </body>
     </html>
   );
