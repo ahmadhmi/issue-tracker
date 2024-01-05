cba8c0c772d9806eb536c4a84100e98f8ddc9b8e
Ahmad Heshmati <amd.heshmati@gmail.com>
2024-01-04 20:36:01 -0700

    Show issue detail

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -11,7 +11,7 @@ const IssuesPage = async () => {
   await delay(2000);
   return (
     <div>
-      <IssueActions/>
+      <IssueActions />
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
@@ -28,13 +28,13 @@ const IssuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-              <IssueStatusBadge status={issue.status}/>
+                <IssueStatusBadge status={issue.status} />
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
                 {issue.createdAt.toDateString()}
