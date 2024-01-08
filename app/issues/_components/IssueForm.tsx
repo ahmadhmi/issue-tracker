f4e8357e61e5dbda2150b47ab7644bb83d9ebe27
Ahmad Heshmati <amd.heshmati@gmail.com>
2024-01-07 20:00:13 -0700

    Fix caching issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -40,6 +40,7 @@ const IssueForm = async ({ issue }: { issue?: Issue }) => {
       if (issue) await axios.patch("/api/issues/" + issue.id, data);
       else await axios.post("/api/issues", data);
       router.push("/issues");
+      router.refresh();
     } catch (error) {
       setIsSubmitting(false);
       setError("An unexpected error occurred.");
